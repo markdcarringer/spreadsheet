12e4f5062002a4c63c4d2062a4bd408eb2e402e2
Carringer <carringermd@corp.saic.com>
2017-12-05 16:24:03 -0500

    Added cell reference resolution.

--- expander.rkt
+++ expander.rkt
@@ -27,8 +27,14 @@
   (define input-list (string-split input))
   (define cell (first input-list))
   (define value (second input-list))
+  
   (lambda (ht ec)
-    (hash-set! ht cell value)
+    ;; Determine whether the value is a cell reference. If so, resolve it into a number or string.
+    ;; If the cell reference is to an unenvalued cell, set the cell to #Error.
+    (define res-value (if (is-cell-ref? value)
+                          (resolve-cell-ref ht value)
+                          value))
+    (hash-set! ht cell res-value)
     (set! ec (cons cell ec))
     (list ht ec)))
 
@@ -36,3 +42,12 @@
   (define new-list (sort (remove-duplicates ec) string<?))
   (for ([key new-list])
     (display (~a key " " (hash-ref ht key) "\n"))))
+
+;; Returns true if the string cell is a valid cell reference, false otherwise. 
+(define (is-cell-ref? cell)
+  (regexp-match? #rx"[A-Za-z]+[0-9]+" cell))
+
+;; This function takes a cell reference and returns either a string or a number, which is the
+;; value of the referenced cell.
+(define (resolve-cell-ref ht ref)
+  (hash-ref ht ref))
